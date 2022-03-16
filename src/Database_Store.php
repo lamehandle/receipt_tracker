bc2f06d6201bfc7c5c3ca0e914cae3f5b28b2e75
lamehandle <jamesrogersgis@gmail.com>
2022-03-15 23:16:33 -0500

    Updated gir with Database_Store.php

---
+++ src/Database_Store.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace app;
+use PDO;
+use PDOException;
+
+require_once 'Store.php';
+
+class Database_Store implements Store
+{
+    public PDO      $conn; // connection
+    public string   $host;
+    public string   $dbname;
+    public string   $port;
+    public string   $charset;
+    public string   $username;
+    public string   $password;
+    public string   $dsn; //data source name
+    public array    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //always throw exceptions
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //retrieve records as associative arrays
+        PDO::ATTR_EMULATE_PREPARES   => false, //do not use emulate mode
+        ];
+
+    public function __construct($host = '', $dbname = '', $port = '', $charset = '', $username = '', $password = ''){
+
+        $this->host     = $host | 'localhost';
+        $this->dbname   = $dbname | 'receipts_tracker';
+        $this->port     = $port | '80';
+        $this->charset  = $charset | 'utf8mb4';
+        $this->username = $username | 'root';
+        $this->password = $password | 'admin';
+        $this->dsn      = "mysql:host=$this->host;dbname=$this->dbname;port=$this->port;charset=$this->charset;";
+
+     }
+
+//     /**
+//        *@param string $username
+//     **/
+//    public function setUsername(string $username): void
+//    {
+//        $this->username = $username;
+//    }
+//
+//    /**
+//        *@param string $dbname
+//    **/
+//    public function setDbname(string $dbname): void
+//    {
+//        $this->dbname = $dbname;
+//    }
+//
+//    /**
+//     * @param string $host
+//     **/
+//    public function setHost(string $host): void
+//    {
+//        $this->host = $host;
+//    }
+
+    public static function connect($dsn, $username, $password, $options) :PDO{
+        try {
+        $conn = new PDO($dsn, $username, $password, $options);
+
+        } catch(PDOException $e){
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+        return $conn;
+    }
+
+    public function retrieve_items($field, $value, Receipt $receipt = null) :Receipt { //todo refactor to be more specific?
+        try {
+            $conn = Database_Store::connect($this->dsn, $this->username, $this->password, $this->options);
+            if($receipt = null){
+                $receipt = new Receipt(uniqid("", false));
+            }
+            $sql = /** @lang text */
+                "SELECT * FROM receipts_2021 WHERE :field = :value";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([
+                    ':field'=> $field,
+                    ':value'=>$value
+                ]);
+            foreach ($stmt as $record){
+                $receipt->addItem($record);
+              }
+        } catch(PDOException $e){
+        throw new PDOException($e->getMessage(), (int)$e->getCode());
+}
+        return $receipt;
+    }
+
+    public function save_item(Line_Item $item){
+        try {
+            $conn = Database_Store::connect($this->dsn, $this->username, $this->password, $this->options);
+            $sql = /** @lang text */
+                "INSERT INTO receipts_2021 (id, vendor, item, category, subtotal, tax, total, date)
+                 VALUES (:id, :vendor, :item, :category, :subtotal, :tax, :total, :date)";
+            $conn->prepare($sql)->execute([
+                'id'        => $item->id,
+                'vendor'    => $item->vendor,
+                'item'      => $item->item,
+                'category'  => $item->category,
+                'subtotal'  => $item->subtotal(),
+                'total'     => $item->total(),
+                'date'      => $item->date
+            ]);
+        } catch(PDOException $e){
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+
+        }
+        $conn = null;
+    }
+
+    public function update_line_item(PDO $conn, Line_Item $item ){
+        try {
+            //connect to db
+            $conn = Database_Store::connect($this->dsn, $this->username, $this->password, $this->options);
+
+            //create and store the query you want to be able to run.
+            $sql = /** @lang text */
+                'UPDATE receipts_2021    
+                SET vendor     = :vendor    AND 
+                    item       = :item      AND 
+                    category   = :category  AND
+                    price      = :price     AND
+                    date       = :date
+                WHERE :id = id';
+            //prepare the sql query. This step prevents injection attacks.
+        $stmt = $conn->prepare($sql);
+            //bind values so that you can dynamically swap variables into the SQL query
+        $stmt->bindValue(':id',         $item->id);
+        $stmt->bindValue(':vendor',     $item->vendor);
+        $stmt->bindValue(':item',       $item->item);
+        $stmt->bindValue(':category',   $item->category);
+        $stmt->bindValue(':price',      $item->price);
+        $stmt->bindValue(':date',       $item->date);
+
+        } catch(PDOException $e){
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+        $conn = null; //close connection
+    }
+
+    public function delete_item(Line_Item $item){
+        try {
+            //connect to db
+            $conn = Database_Store::connect($this->dsn, $this->username, $this->password, $this->options);
+
+            $sql = /** @lang text */
+                "Delete FROM receipts_2021 
+                 WHERE :id = id";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(':id', $item->id);
+            $stmt->execute();
+
+        } catch(PDOException $e){
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+        $conn = null;
+
+    }
+
+
+}
---
+++ src/main_test_2.php
@@ -0,0 +1,121 @@
+<?php
+namespace app;
+use DateTime;
+
+require __DIR__ . '/vendor/autoload.php';
+require_once('Purchase_Record.php');
+require_once('Receipt.php');
+require_once('Line_Item.php');
+require_once('Utilities.php');
+require_once('Filter.php');
+require_once('Report.php');
+$tax_rates_1 = ["GST"=> 0.01, "PST"=> 0.01 ];
+$tax_rates_2 = ["GST"=> 0.20];
+$tax_rates_3 = ["PST"=> 0.07, "VAT" => 0.1];
+
+$date = new DateTime();
+
+//test adding arrays of categories to a category list
+//$new_category_test_1 = ["Lasers?","Bombs?"];
+//$new_category_test_2 = ["Kittens","Puppies","Snakes"];
+//$new_category_test_3 = ["Kittens","Puppies","Snakes!", "Knives!"];
+
+
+echo "\n";
+echo "file is powering up... \n";
+echo "------------------------------------------------------------------------------------- \n";
+echo "\n";
+
+echo "\n";
+echo "Creating line items...";
+echo "\n";
+echo "\n";
+
+$test_input_1 = new Line_Item(uniqid("", false), "test-vendor-1",
+    "test-item-1","Kittens", $tax_rates_1,Utilities::random_price(), $date);
+
+$test_input_2 = new Line_Item(uniqid("", false), "test-vendor-2",
+    "test-item-2","Lasers", $tax_rates_2,Utilities::random_price(), $date);
+
+$test_input_3 = new Line_Item(uniqid("", false), "test-vendor-3",
+    "test-item-3","Bombs", $tax_rates_3,Utilities::random_price(), $date);
+
+
+echo "------------------------------------------------------------------------------------- \n";
+echo "Initializing receipt...";
+echo "\n";
+echo "\n";
+
+$receipt = new Receipt(uniqid("", false));
+
+//echo "------------------------------------------------------------------------------------- \n";
+//echo "Initializing line item 1...";
+//echo "\n";
+//echo "\n";
+$receipt->addItem($test_input_1);
+
+//print_r($receipt);
+
+//echo "------------------------------------------------------------------------------------- \n";
+//echo "Initializing line item 2...";
+//echo "\n";
+//echo "\n";
+$receipt->addItem($test_input_2);
+
+//print_r($receipt);
+
+//echo "------------------------------------------------------------------------------------- \n";
+//echo "Initializing line item 3...";
+//echo "\n";
+//echo "\n";
+$receipt->addItem($test_input_3);
+
+//print_r($receipt);
+
+echo "------------------------------------------------------------------------------------- \n";
+$filtered_by_vendor = Filter::filter_by_vendor($receipt, "test-vendor-1");
+echo "Here's is the result of vendor filter: ";
+echo "\n";
+var_dump($filtered_by_vendor->line_items);
+echo "\n";
+echo "End Vendor filter test.";
+echo "\n";
+echo "------------------------------------------------------------------------------------- \n";
+
+Filter::clear_filter($filtered_by_vendor);
+
+echo "Here's the result of category filter: ";
+echo "\n";
+$filtered_by_category = Filter::filter_by_category($receipt, "Bombs");
+var_dump($filtered_by_category->line_items);
+echo "\n";
+echo "End Category filter test.";
+echo "------------------------------------------------------------------------------------- \n";
+echo "\n";
+
+echo "Here's the result of item name filter: ";
+echo "\n";
+$filtered_by_name = Filter::filter_by_item_name($receipt, "test-item-2");
+var_dump($filtered_by_name->line_items);
+echo "\n";
+echo "End item name filter test.";
+echo "------------------------------------------------------------------------------------- \n";
+echo "\n";
+
+echo "------------------------------------------------------------------------------------- \n";
+echo "Receipt subtotal: ";
+print_r($receipt->subtotal());
+echo "\n";
+echo "------------------------------------------------------------------------------------- \n";
+echo "Receipt taxes: ";
+echo "\n";
+foreach ($receipt->taxes() as $tax) {
+    echo $tax . "\n";
+}
+echo "\n";
+echo "------------------------------------------------------------------------------------- \n";
+echo "Receipt final total: ";
+echo $receipt->total();
+echo "\n";
+echo "------------------------------------------------------------------------------------- \n";
+
